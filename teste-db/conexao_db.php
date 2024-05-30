3c0622d8b1eef97f94bb2037a174464d636755a8
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-05-30 14:34:23 -0400

    começando a migração do projeto para o laravel

--- teste-db/conexao_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-    $conexao = new PDO('mysql:host=localhost;dbname=banco_lucas_projetos', 'root', '312006');
-    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $erro) {
-    echo "Erro: " . $erro->getMessage();
-}
